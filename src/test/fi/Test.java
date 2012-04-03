1788b58e20bb7e3257390e7bdb0afc2a49b720b5
Jukka Edvardsson <jukka.edvardsson@gmail.com>
2012-04-03 18:27:55 +0300

    test commit

--- src/test/fi/Test.java
+++ src/test/fi/Test.java
@@ -10,6 +10,6 @@ package test.fi;
 public class Test {
     
     public static void main (String [] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!!");
     }
 }
