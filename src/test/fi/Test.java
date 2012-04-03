d3d65c657c7cbdac6226ef64b75465f01d9fb18d
Jukka Edvardsson <jukka.edvardsson@gmail.com>
2012-04-03 18:15:16 +0300

    First commit

---
+++ src/test/fi/Test.java
@@ -0,0 +1,15 @@
+package test.fi;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Malli
+ * Date: 3.4.2012
+ * Time: 18.08
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    
+    public static void main (String [] args) {
+        System.out.println("Hello world");
+    }
+}
